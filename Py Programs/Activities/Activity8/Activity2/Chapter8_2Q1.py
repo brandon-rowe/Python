e38e42f2d923224e0a1492a83a715fc013ac8309
brandon-rowe <browe47@gmail.com>
2019-02-21 18:05:50 -0500

    chdir struct

--- Py Programs/Activities/Activity8/Activity2/Chapter8_2Q1.py
+++
@@ -1,9 +0,0 @@
-# Chapter 8
-# Activiy 2
-# Question 1
-def main():
-    sample = [(10,20,40),(40,50,60),(70,80,90)]
-    for str in sample:
-        str = (str[0]+ str[1] + 100)
-    print(sample)
-main()
