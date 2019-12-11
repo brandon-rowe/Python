6d18250928b0b92cc68ee4a044e148a960ddcbee
brandon-rowe <brandon@cyntax.org>
2019-12-10 23:10:12 -0500

    Check

--- Py Programs/Encryption/readFile.py
+++ Py Programs/Encryption/readFile.py
@@ -12,24 +12,3 @@ def main():
     print("There are %s words, %s lines and %s characters in your file." % (words, lines, characters))
 main()
 
-
-#print("There are ",words," words in your file.")
-#print("There are ",lines," lines in your file.")
-#print("There are ",characters," characters in your file.")
-
-
-#data = readFile.read()
-#    words = data.split()
-#    count = 0
-#    for words in words:
-#        count+=1
-#    print("There are ",count," words in your file.")
-#    countLine = 0
-#    line = data.split('.')
-#    for line in line:
-#        countLine+=1
-#    print("There are ",countLine," lines in your file.")
-#    countChars = 0
-#    for chars in data:
-#        countChars+=1
-#    print("There are ",countChars," characters in your file.")
