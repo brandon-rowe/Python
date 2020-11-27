6152b33f1174f118a2d67bf35f44745c828fa1ca
brandon-rowe <brandon@cyntax.org>
2020-11-26 22:01:34 -0500

    Playing with bash

---
+++ Projects/Schedule/hello007.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Hello 007
+
+echo "Hello 007"
+
+python hello007.py
--- Projects/Schedule/scripting/hello007.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Hello Agent 007
-
-echo "Hello 007"
-
-for i in $(cat ~/Desktop/todo.txt)
-do
-	echo $i
-done
